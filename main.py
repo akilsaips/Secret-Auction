5e3e82ee5afdd096d8b8529ba240ba2cbf6d8aca
Akilsai <86817062+akilsaips@users.noreply.github.com>
2023-10-31 22:55:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from replit import clear
+
+from art import logo
+print(logo)
+
+game = True
+data = []
+players = [] 
+winner = ""
+
+while game:
+  name = input("Name: ")
+  bid = int(input("Bid: $"))
+  
+  newdata = {"name":name, "bid":bid}
+  data.append(newdata)
+
+  choice = input("Do you want to continue?(Yes/No): ").lower()
+
+  if choice == "yes":
+    clear()
+  else:
+    for dick in data:
+      for key in dick:
+        players.append(dick["bid"])
+    game = False
+
+points = max(players)
+
+for n in data:
+  if n["bid"] == points:
+    winner = n["name"]
+    break
+
+print(f"The winner is {winner} who bid ${points}")
